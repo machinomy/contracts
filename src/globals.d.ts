002bafe4badc49d7b7a20559d85cbd7c1dcd1467
Sergey Ukustov <sergey@ukstv.me>
2017-12-27 03:40:28 +0300

    Add method declarations to ethereumjs-abi

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -2,5 +2,7 @@
 declare var contract: (name: string, callback: (accounts: Array<string>) => void) => void
 
 declare module 'ethereumjs-abi' {
-  export function soliditySHA3(types: string[], values: any[]): Buffer
+  export function soliditySHA3 (types: string[], values: any[]): Buffer
+  export function rawEncode (types: string[], values: any[]): Buffer
+  export function rawDecode<A> (types: string[], data: Buffer): A
 }
